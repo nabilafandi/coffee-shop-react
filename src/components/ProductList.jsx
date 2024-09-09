63c6b46d181129f658de4e52f01ca8d707ad1eb7
nabilafandi <nabilafandih@gmail.com>
2024-09-09 18:23:11 +0700

    feat(shop): desktop view

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,38 +2,38 @@
 
 const Product = ({ product }) => {
   return (
-    <div key={product.id} className="group relative">
-      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200  group-hover:opacity-75 ">
-        <img
-          alt={product.imageAlt}
-          src={product.imageSrc}
-          className="h-full w-full object-cover object-center "
-        />
-      </div>
-      <div className="mt-4 flex justify-between">
-        <div>
-          <h3 className="text-sm text-gray-700">
-            <a href={product.href}>
-              <span aria-hidden="true" className="absolute inset-0" />
-              {product.name}
-            </a>
-          </h3>
-          <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+      <div key={product.id} className="group relative">
+        <div className="w-full aspect-square overflow-hidden rounded-md bg-gray-200  group-hover:opacity-75 ">
+          <img
+            alt={product.imageAlt}
+            src={product.photo_md}
+            className="h-full w-full object-cover object-center "
+          />
         </div>
-        <p className="text-sm font-medium text-trippicalBlack">{product.price}</p>
+        <div className="mt-4 flex justify-between">
+          <div>
+            <h3 className="text-sm font-semibold text-trippicalBlack">
+              <a href={product.href}>
+                <span aria-hidden="true" className="absolute inset-0" />
+                {product.name}
+              </a>
+            </h3>
+            <p className="mt-1 text-sm text-trippicalBlack">{product.color}</p>
+          <p className="text-sm font-semibold text-logoRed ">IDR {Math.round(product.min_sell_price / 1000) } / {Math.round(product.max_sell_price / 1000)} </p>
+          </div>
+        </div>
       </div>
-    </div>
   );
 };
 const ProductList = ({ items }) => {
   return (
     <div className="bg-offWhite">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24">
-        <h2 className="text-2xl font-bold tracking-tight text-trippicalBlack">
+      <div className="mx-auto max-w-2xl lg:max-w-full px-4 py-16 sm:px-6 sm:py-24">
+        <h2 className="text-2xl font-mogena font-bold tracking-tight text-trippicalBlack">
           {items.name}
         </h2>
 
-        <div className="mt-6 grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-2 ">
+        <div className="mt-6 grid grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-10 sm:grid-cols-2 ">
           {items.products.map((item) => (
             // eslint-disable-next-line react/jsx-key
             <Product product={item}/>
